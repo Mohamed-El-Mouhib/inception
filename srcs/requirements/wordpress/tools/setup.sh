eb61b2619e89e8bbe73df4d0a28f36b632486b0e
Mohamed El mouhib <huntermed15@gmail.com>
2026-08-17 19:42:23 +0100

    adding mariadb client ping for preventing wordpress running before mariadb

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -3,10 +3,10 @@
 # 1. Check if WordPress configuration exists
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     
-    # echo "Waiting for MariaDB to be ready..."
-    # while ! mariadb-admin ping -h"mariadb" -u"root" -p"mq71sg" --silent; do
-    #     sleep 2
-    # done
+    echo "Waiting for MariaDB to be ready..."
+    while ! mariadb-admin ping -h"mariadb" -u"root" -p"mq71sg" --silent; do
+        sleep 2
+    done
 
     echo "Downloading WordPress..."
     wp core download --allow-root
@@ -31,18 +31,11 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
         --skip-email \
         --allow-root
 
-	echo "php_admin_value[error_log] = /proc/1/fd/2" >> /etc/php83/php-fpm.d/www.conf
-	echo "access.log = /proc/1/fd/1" >> /etc/php83/php-fpm.d/www.conf
-        
     echo "WordPress successfully installed!"
 else
     echo "WordPress is already installed. Skipping initialization."
 fi
 
 chown -R www-data:www-data /var/www/html
-# 4. Hand off execution back to the main container process (PHP-FPM)
-# No quotes around the command and its arguments!
-#
 
 exec php-fpm83 -F
-# exec nc -l 0.0.0.0  9000 
