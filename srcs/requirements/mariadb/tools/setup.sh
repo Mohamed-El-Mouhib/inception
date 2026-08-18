6c5435f73ba4bf3e3efaa6d98295b82bc50d75d1
Mohamed El mouhib <huntermed15@gmail.com>
2026-08-18 21:47:59 +0100

    simple update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -8,15 +8,16 @@ mkdir -p /run/mysqld /var/lib/mysql \
 
 # echo "========================="
 # echo "ROOT_PWD:" $ROOT_PWD
+# echo "ADMIN_PWD:" $ADMIN_PWD
+#
 # echo "DB_NAME:" $DB_NAME
 # echo "ADMIN_LOGIN:" $ADMIN_LOGIN
-# echo "ADMIN_PWD:" $ADMIN_PWD
 # echo "========================="
 
 # making sure that if the /var/lib/mysql/mysql doesn't exist it re initiate DB tables and grants
 # the user which is mariaDB, the ownership of that directory so the DB works fine
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "[info] MySQL data directory not found. Initializing database..."
+    echo "MySQL data directory not found. Initializing database..."
 
     # Initialize the system DB
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
@@ -30,9 +31,9 @@ GRANT ALL ON \`$DB_NAME\`.* TO '$ADMIN_LOGIN'@'%';
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES;
 EOF
-    echo "[info] Database initialized."
+    echo "Database initialized."
 fi
 
 # Start MariaDB server, listening on all interfaces (skip-name-resolve speeds up internal lookups)
-echo "[info] Starting MariaDB Server..."
+echo "Starting MariaDB Server..."
 exec mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking=0 --bind-address=0.0.0.0
