f332d17c4d0939cf08c9bb2dc59c3698d76f96da
Mohamed El mouhib <huntermed15@gmail.com>
2026-08-18 21:47:34 +0100

    waitiing till mariadb service is running

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,41 +1,60 @@
 #!/bin/sh
 
-# 1. Check if WordPress configuration exists
+# Check if mariadb is running before wordpress so we can run create/access the site
+echo "Waiting for MariaDB to be ready..."
+while ! mariadb-admin ping -h"mariadb" -u"root" -p"mq71sg" --silent; do
+	sleep 2
+done
+
+# check if wordpress configuration exists
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     
-    echo "Waiting for MariaDB to be ready..."
-    while ! mariadb-admin ping -h"mariadb" -u"root" -p"mq71sg" --silent; do
-        sleep 2
-    done
-
     echo "Downloading WordPress..."
-    wp core download --allow-root
+    wp core download --allow-root --force
     echo "Creating wp-config.php..."
 
-    # 2. Create the configuration file linking to MariaDB
+    # create the configuration file linking to mariadb
     wp config create \
-        --dbname="wp_db" \
-        --dbuser="root" \
-        --dbpass="mq71sg" \
-        --dbhost="mariadb" \
-        --allow-root
+	    --dbname="wp_db" \
+	    --dbuser="root" \
+	    --dbpass="mq71sg" \
+	    --dbhost="mariadb" \
+	    --allow-root
 
     echo "Installing WordPress..."
-    # 3. Execute the automated install
-    wp core install \
-        --url="localhost" \
-        --title="inception" \
-        --admin_user="wp_drake" \
-        --admin_password="wp_pass" \
-        --admin_email="befdrake@inception.com" \
-        --skip-email \
-        --allow-root
 
+    # execute the automated install
+    wp core install \
+	    --url="localhost" \
+	    --title="inception" \
+	    --admin_user="wp_drake" \
+	    --admin_password="wp_pass" \
+	    --admin_email="wp_drake@example.com" \
+	    --skip-email \
+	    --allow-root
+
+    # create a standard user 
+    wp user create \
+	    user_ user_@42.fr \
+	    --role=author \
+	    --user_pass=randomPass \
+	    --allow-root
+ 
     echo "WordPress successfully installed!"
 else
-    echo "WordPress is already installed. Skipping initialization."
+	if ! wp core is-installed --allow-root 2> /dev/null; then
+		echo "Config exists but database is empty. Running installation..."
+		wp core install \
+			--url="localhost" \
+			--title="inception" \
+			--admin_user="wp_drake" \
+			--admin_password="wp_pass" \
+			--admin_email="wp_drake@example.com" \
+			--skip-email \
+			--allow-root
+	else
+		echo "WordPress is already installed. Skipping initialization."
+	fi
 fi
-
-chown -R www-data:www-data /var/www/html
 
 exec php-fpm83 -F
