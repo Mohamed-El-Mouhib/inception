6b05aeb4c89e84726b84146d8b015f83d28dfc8c
Mohamed El mouhib <huntermed15@gmail.com>
2026-09-10 18:13:11 +0100

    refine the environment variables naming and less passing the secrets to containers

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,37 +1,11 @@
 #!/bin/sh
 
-# "========ENV======="
-#
-# $HOSTNAME       == localhost
-# $MARIADB        == mariadb
-# $DB_NAME        == wp_db
-# $WP_TITLE       == inception
-# $WP_ADMIN_MAIL  == drake@example.com
-# $WP_USER        == mel-mouh
-# $WP_USER_MAIL   == mel-mouh@42.fr
-# $WP_USER_ROLE   == author
-#
-# "========SECRETS======="
-#
-# $DB_ROOT_USER   == root
-# $DB_ROOT_PWD    == root4ever
-# $WP_ADMIN_LOGIN == befdrake
-# $WP_ADMIN_PWD   == born2die
-# $WP_USER_PWD    == randomPass
-#
-# "========================="
-
-DB_ROOT_USER=$(cat /run/secrets/db_root_user)
-DB_ROOT_PWD=$(cat /run/secrets/db_root_pwd)
-WP_ADMIN_USER=$(cat /run/secrets/wp_admin_login)
-WP_ADMIN_PWD=$(cat /run/secrets/wp_admin_pwd)
-WP_USER_PWD=$(cat /run/secrets/wp_admin_pwd)
-
+DB_PWD=$(cat /run/secrets/db_password.txt)
 
 # Check if mariadb is running before wordpress so we can run create/access the site
 # ENV instead of hardcoded crendetials
 echo "Waiting for MariaDB to be ready..."
-while ! mariadb-admin ping -h"$MARIADB" -u"${DB_ROOT_USER}" -p"${DB_ROOT_PWD}" --silent; do
+while ! mariadb-admin ping -h"$MARIADB" -u"$DB_USER" -p"${DB_PWD}" --silent; do
 	sleep 2
 done
 
@@ -44,10 +18,10 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
     # create the configuration file linking to mariadb
     wp config create \
-	    --dbname="$DB_NAME"        \
-	    --dbuser="${WP_ADMIN_USER}" \
-	    --dbpass="${WP_ADMIN_PWD}"  \
-	    --dbhost="$MARIADB"        \
+	    --dbname="$DB_NAME" \
+	    --dbuser="$DB_USER" \
+	    --dbpass="${DB_PWD}"\
+	    --dbhost="$MARIADB" \
 	    --allow-root
 
     echo "Installing WordPress..."
@@ -56,17 +30,17 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
     wp core install \
 	    --url="$HOSTNAME" \
 	    --title="$WP_TITLE" \
-	    --admin_user="${WP_ADMIN_USER}" \
-	    --admin_password="${WP_ADMIN_PWD}" \
+	    --admin_user="$WP_ADMIN_USER" \
+	    --admin_password="$WP_ADMIN_PWD" \
 	    --admin_email="$WP_ADMIN_MAIL" \
 	    --skip-email \
 	    --allow-root
 
     # create a standard user 
     wp user create \
-	    $WP_USER $WP_USER_MAIL \
-	    --role=$WP_USER_ROLE \
-	    --user_pass=${WP_USER_PWD} \
+	    "$WP_USER" "$WP_USER_MAIL" \
+	    --role="$WP_USER_ROLE"     \
+	    --user_pass="$WP_USER_PWD" \
 	    --allow-root
  
     echo "WordPress successfully installed!"
@@ -76,8 +50,8 @@ else
 		wp core install \
 			--url="$HOSTNAME" \
 			--title="$WP_TITLE" \
-			--admin_user="${WP_ADMIN_USER}" \
-			--admin_password="${WP_ADMIN_PWD}" \
+			--admin_user="$WP_ADMIN_USER" \
+			--admin_password="$WP_ADMIN_PWD" \
 			--admin_email="$WP_ADMIN_MAIL" \
 			--skip-email \
 			--allow-root
